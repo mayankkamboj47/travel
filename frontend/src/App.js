8b6fecc2217c71fd54d7e9c09b9ef6b258e01b13
Mayank Kamboj <mayankkamboj47@gmail.com>
2022-01-05 12:36:29 +0530

    Add detailsCard
    1. Get the airbnb data from JSON. It is unparseable for now.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,10 @@
 import {
-  Container, Heading, Text, Flex,
+  Container, Heading, Text, Flex, Spinner,
 } from '@chakra-ui/react';
 import React from 'react';
 import Hero from './Hero';
 import Nav from './Nav';
-import ImageCard from './Card';
+import { ImageCard, DetailsCard } from './Card';
 import useRemote from './hooks';
 
 function App() {
@@ -15,6 +15,39 @@ function App() {
       <Container maxW={1600}>
         <Places />
         <About />
+        <DetailsCard
+          image="https://a0.muscache.com/pictures/4686147/5b0c1802_original.jpg"
+          title="Amsterdam Apartment + Southgarden"
+          caption="Entire home/apartment in Noord-Holland"
+          rating={4.3}
+          reviews={47}
+          price="$49.0"
+          amenities={['Refrigerator',
+            'Piano',
+            'Dishes and silverware',
+            'Heating',
+            'Hair dryer',
+            'Body soap',
+            'Wifi',
+            'Shampoo']}
+        />
+        <DetailsCard
+          image="https://a0.muscache.com/pictures/4686147/5b0c1802_original.jpg"
+          title="Amsterdam Apartment + Southgarden"
+          caption="Entire home/apartment in Noord-Holland"
+          rating={4.3}
+          reviews={47}
+          price="$49.0"
+          amenities={['Refrigerator',
+            'Piano',
+            'Dishes and silverware',
+            'Heating',
+            'Hair dryer',
+            'Body soap',
+            'Wifi',
+            'Shampoo']}
+        />
+
       </Container>
     </>
   );
@@ -23,7 +56,9 @@ function App() {
 export default App;
 
 function Places() {
-  const [data] = useRemote('http://localhost:8080/places');
+  const [data, loading, error] = useRemote('http://localhost:8080/places');
+  if (loading) return <Spinner />;
+  if (error) return <Heading>Something went wrong</Heading>;
   return (
     <>
       <Heading my="1rem">Places</Heading>
@@ -37,7 +72,6 @@ function Places() {
               subtitle={country}
             />
           ),
-
         )
       }
       </Flex>
--- frontend/src/Card.js
+++ frontend/src/Card.js
@@ -1,7 +1,11 @@
-import { Heading, Flex, Text } from '@chakra-ui/react';
+import {
+  Heading, Flex, Text, Image, Box, IconButton,
+} from '@chakra-ui/react';
+import { faHeart, faStar } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React from 'react';
 
-export default function ImageCard({ image, title, subtitle }) {
+export function ImageCard({ image, title, subtitle }) {
   const styles = {
     color: 'white',
     borderRadius: '0.5rem',
@@ -11,6 +15,54 @@ export default function ImageCard({ image, title, subtitle }) {
     <Flex background={`url(${image})`} backgroundSize="cover" backgroundPosition="center" flexDirection="column" p="2rem" minH="300px" style={styles} minW="200px" justifyContent="end">
       <Heading>{title}</Heading>
       <Text>{subtitle}</Text>
+    </Flex>
+  );
+}
+
+export function DetailsCard({
+  image, title, caption, rating, reviews, price, amenities,
+}) {
+  const priceBoxStyle = {
+    position: 'absolute',
+    bottom: 0,
+    right: 0,
+  };
+  const ratingBoxStyle = {
+    position: 'absolute',
+    bottom: 0,
+    left: 0,
+  };
+  const heartStyle = {
+    position: 'absolute',
+    top: 0,
+    right: 0,
+  };
+  return (
+    <Flex w="100%" maxW="50rem" gap="1rem" style={{ lineHeight: 1.8 }} py="1rem">
+      <Image src={image} flex={1} maxW="20rem" />
+      <Box flex={1} position="relative">
+        <Text fontSize="sm" color="gray.500">{caption}</Text>
+        <Heading size="md">{title}</Heading>
+        {amenities.map(
+          // eslint-disable-next-line comma-dangle
+          (amenity) => <Text as="span" mr="1rem" wordBreak="keep-all" fontSize="sm" color="gray.500">{amenity}</Text>
+        )}
+        <Text style={ratingBoxStyle}>
+          <FontAwesomeIcon icon={faStar} />
+          {' '}
+          {rating}
+          {' '}
+          (
+          {reviews}
+          {' '}
+          Reviews)
+        </Text>
+        <Text style={priceBoxStyle}>
+          <strong>{price}</strong>
+          /night
+        </Text>
+        <IconButton icon={<FontAwesomeIcon icon={faHeart} />} style={heartStyle} />
+      </Box>
     </Flex>
   );
 }
