830780f521ec0204e0217d533f0a1c98953f34d5
Ashwin0501 <ashwin.salampuria_ug23@ashoka.edu.in>
2022-01-26 12:29:20 +0000

    set imagegrid on HotelPage

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,6 +8,16 @@ import Search from './Search';
 import HotelPage from './HotelPage';
 
 function App() {
+  const hotelData= {"rooms": {"guests": 16.0, "bedrooms": 5.0, "beds": 5.0, "bathrooms": 6.5}, 
+  "amenities": ["Wifi", "Kitchen", "Free parking"],
+   "title": "CasaMo, Silver Oak", 
+   "rating": 4.67,
+   "reviews" :  6, 
+   "images": ["https://a0.muscache.com/im/pictures/miso/Hosting-31084171/original/55c6dc97-3d6e-43b8-af2c-5a3501efc8cc.jpeg?im_w=720", "https://a0.muscache.com/im/pictures/miso/Hosting-31084171/original/5e9616c4-c6cf-4433-89bc-d5f213324320.jpeg?im_w=720", "https://a0.muscache.com/im/pictures/miso/Hosting-31084171/original/653db8f7-8884-4369-b700-f4334c272166.jpeg?im_w=720", "https://a0.muscache.com/im/pictures/miso/Hosting-31084171/original/75903290-e051-4d5b-bce2-b6818fd7dedf.jpeg?im_w=720", "https://a0.muscache.com/im/pictures/miso/Hosting-31084171/original/9f7a0843-f3a2-4cdc-86da-2593c9644373.jpeg?im_w=720"],
+   "price": "\u20b935,200",
+   "description" : "This is the best hotel there is period",
+   "location" : "Mahabaleshwar, Maharashtra, India"}
+
   return (
     <BrowserRouter>
       <Nav />
@@ -16,7 +26,8 @@ function App() {
         <Route path="/card" element={<Cards />} />
         <Route path="/profile" element={<Profile />} />
         <Route path="search/:query" element={<Search />} />
-        <Route path="/hotelpage"><HotelPage /></Route>
+        <Route path="/hotelpage" element={<HotelPage title={hotelData.title} rating={hotelData.rating} 
+        reviews={hotelData.reviews} location= {hotelData.location} images= {hotelData.images} description={hotelData.description} />} />
       </Routes>
     </BrowserRouter>
   );
--- frontend/src/HotelPage.js
+++ frontend/src/HotelPage.js
@@ -19,7 +19,16 @@ export default function HotelPage({
     </React.Fragment>
   );
 }
-
+/*{"rooms": {"guests": 16.0, "bedrooms": 5.0, "beds": 5.0, "bathrooms": 6.5}, 
+"amenities": ["Wifi", "Kitchen", "Free parking"],
+ "title": "CasaMo, Silver Oak", 
+ "rating": 4.67,
+ "reviews" :  6, 
+ "images": ["https://a0.muscache.com/im/pictures/miso/Hosting-31084171/original/55c6dc97-3d6e-43b8-af2c-5a3501efc8cc.jpeg?im_w=720", "https://a0.muscache.com/im/pictures/miso/Hosting-31084171/original/5e9616c4-c6cf-4433-89bc-d5f213324320.jpeg?im_w=720", "https://a0.muscache.com/im/pictures/miso/Hosting-31084171/original/653db8f7-8884-4369-b700-f4334c272166.jpeg?im_w=720", "https://a0.muscache.com/im/pictures/miso/Hosting-31084171/original/75903290-e051-4d5b-bce2-b6818fd7dedf.jpeg?im_w=720", "https://a0.muscache.com/im/pictures/miso/Hosting-31084171/original/9f7a0843-f3a2-4cdc-86da-2593c9644373.jpeg?im_w=720"],
+ "price": "\u20b935,200",
+ "description" : "This is the best hotel there is period"
+ "location" : "Mahabaleshwar, Maharashtra, India"}
+ */
 function RatingReviewsAndLocation({ rating, reviews, location }) {
   return (
     <Flex>
@@ -40,12 +49,17 @@ function RatingReviewsAndLocation({ rating, reviews, location }) {
 
 function ImagesHero({ images }) {
   const containerStyle = {
+    display: 'grid',
     gridTemplateColumns: '2fr 1fr 1fr',
     gridTemplateRows: '1fr 1fr',
   };
   const mainImageStyle = {
-    gridRow: '1/3',
-    gridColumn: '1/2',
+    gridRowStart: 1,
+    gridRowEnd: 3,
+    gridColumnStart:1,
+    gridColumnEnd: 2,
+    width: '100%',
+    height: '100%',
   };
   return (
     <div style={containerStyle}>
