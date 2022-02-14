242d5b5f6ef0c2dbe10ca22132b01d13755d49b5
Kanishk and Co <karan.handa.fox@gmail.com>
2022-02-14 19:51:24 +0530

    .env committed

---
+++ backend/routes/user.js
@@ -0,0 +1,31 @@
+const express = require('express')
+const router = express.Router()
+const User = require('../models/User')
+
+router.get('/', (req, res) => {
+    const user = req.user || null
+    res.json(user)
+})
+
+router.put('/wishlist', async (req, res) => {
+    if (!req.body.hotel) {
+        return res.status(400).send(null)
+    }
+    
+    const user = req.user || null
+    
+    if (!user) {
+        return res.status(403).send(null)
+    }
+
+    try {
+        const updateduser = await User.findOneAndUpdate({name: user.name}, {$push : {wishlist: req.body.hotel}})
+        return res.status(200).send(null)
+    } catch(err) {
+        console.log(err)
+        return res.status(500).send(err)
+    }
+})
+
+module.exports = router
+
